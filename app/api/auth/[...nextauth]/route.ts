b593bcadd19a393f2e8149c11a2d02fe42fe6321
tobzbobz <tobypage3011@gmail.com>
2025-12-02 05:10:23 +1300

    fix pls

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,3 @@
-import NextAuth from "next-auth"
-import Discord from "next-auth/providers/discord"
+import { handlers } from "@/auth"
 
-const handler = NextAuth({
-  providers: [
-    Discord({
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub!
-      }
-      return session
-    },
-  },
-})
-
-export { handler as GET, handler as POST }
+export const { GET, POST } = handlers
---
+++ auth.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import Discord from "next-auth/providers/discord"
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub!
+      }
+      return session
+    },
+  },
+})
