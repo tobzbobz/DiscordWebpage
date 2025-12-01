54f22fcc49345582b6afaad2e91225acd9a5a447
tobzbobz <tobypage3011@gmail.com>
2025-12-02 06:11:00 +1300

    fix

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Quick debug route for deployed builds to check environment and package info
+app.get('/api/debug', (req, res) => {
+  const pkg = require('./package.json');
+  res.json({
+    ok: true,
+    name: pkg.name,
+    version: pkg.version,
+    node: process.version,
+    platform: process.platform,
+    env: {
+      client_id_set: Boolean(process.env.CLIENT_ID),
+      client_secret_set: Boolean(process.env.CLIENT_SECRET),
+      vercel_url: process.env.VERCEL_URL || null,
+      vercel_env: process.env.VERCEL_ENV || null,
+    }
+  });
+});
+
 module.exports = app;
 
 // Only listen when started directly (not in serverless environments)
