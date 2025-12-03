f804c7324d032ffe731ae20a486b3b1cf8c43e24
tobzbobz <tobypage3011@gmail.com>
2025-12-03 20:07:46 +1300

    fixxxx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useSearchParams, useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const dynamic = 'force-dynamic'
 
