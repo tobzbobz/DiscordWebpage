1cd6414c3e542d33794cc26fa1160d8f830c95b3
tobzbobz <tobypage3011@gmail.com>
2025-12-03 17:47:32 +1300

    hello

--- server.js
+++
@@ -1,3 +0,0 @@
-
-require('dotenv').config();
-// server.js is deprecated after conversion to Next.js; please run `npm run dev` for Next.js local server
--- utils.js
+++
@@ -1,4 +0,0 @@
-
-// Deprecated: utility functions are no longer needed after migrating to Next.js API routes.
-// Keep file here as a reference.
-module.exports = {};
